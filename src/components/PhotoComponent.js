91cbcd398be3166af623c51c8b8ac90af6324bcb
TheHaunted <kevin_lopez@live.com>
2019-07-18 16:37:09 -0500

    MVP

--- src/components/PhotoComponent.js
+++ src/components/PhotoComponent.js
@@ -1,3 +1,13 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 
+function PhotoComponent(props) {
+    return (
+        <div>
+            <img src={props.url}/>
+            <h1>{props.title}</h1>
+            <p>{props.explanation}</p>
+        </div>
+    )
+}
+
+export default PhotoComponent;
